95a055b8405a0ed975714b5ea706435c54cd5dd6
erickim713 <erickim713@gmail.com>
2017-07-01 19:21:37 +0900

    company name, description acquired

--- quotes.py
+++ quotes.py
@@ -8,10 +8,13 @@ class QuotesSpider(scrapy.Spider):
     ]
 
     def parse(self, response):
-        for content in response.css('div.company-name'):
+        for content in response.css('div.company.item'):
             yield {
-                'company-name': content.css('a > h4.header.name > strong::text').extract_first()
-                # 'author': quote.xpath('span/small/text()').extract_first(),
+            # company name, description of company, available spots
+                'company-name': content.css('div.content > div.company-name > a > h4.header.name > strong::text').extract_first(),
+                'description': content.css('div.content > div.description::text').extract_first(),
+                # 'availability': content.css('div.content > div.company-jobs > div.company-job.transition.visible > span.nowrap.job-title').extract_first(),
+                # 'job-info': content.css('div.content > div.company-jobs > div.company-job.transition.visible > span.job-stat-info').extract_first()
             }
         # next_page = response.css('li.next a::attr("href")').extract_first()
         # if next_page is not None:
--- readfile.py
+++ readfile.py
@@ -3,4 +3,7 @@ from pprint import pprint
 
 with open('quotes.json') as data_file:
     data = json.load(data_file)
-print(data[0]['company-name'])
+
+for entry in data:
+    print("회사:" + entry['company-name'])
+    print("설명:" + entry['description'])
